335c3f7a9971016c2dce9bbef78f9084e71026f9
dr-dzhevaga <dr.dzhevaga@gmail.com>
2015-09-15 00:07:25 +0400

    Parse request with common parsings

--- src/main/java/org/mb/http/MainHandler.java
+++ src/main/java/org/mb/http/MainHandler.java
@@ -1,9 +1,15 @@
 package org.mb.http;
 
 import com.google.common.collect.Maps;
+import com.google.common.collect.Table;
 import org.mb.http.basic.Request;
 import org.mb.http.basic.Response;
 import org.mb.http.basic.Handler;
+import org.mb.http.mapping.HandlerDataMapping;
+import org.mb.parsing.Parser;
+import org.mb.parsing.ParserFactory;
+import org.mb.parsing.InputFormat;
+import org.mb.parsing.ParsingException;
 import org.mb.settings.Settings;
 
 import java.util.Map;
@@ -20,7 +26,19 @@ public class MainHandler implements Handler {
 
     @Override
     public Response handle(Request request) {
-        Map<String, String> content = Maps.newHashMap();
-        return settings.getMapping().find(request, content).getResponse();
+        Map<String, String> parsingResults = Maps.newHashMap();
+
+        Table<InputFormat, String, String> parsing = settings.getParsing();
+        for(InputFormat inputFormat : parsing.rowKeySet()) {
+            Parser parser = ParserFactory.newParser(inputFormat, request.getContent());
+            try {
+                parsingResults.putAll(parser.parse(parsing.row(inputFormat)));
+            } catch (ParsingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        HandlerDataMapping.HandlerData handlerData = settings.getMapping().find(request, parsingResults);
+        return handlerData.getResponse();
     }
 }
